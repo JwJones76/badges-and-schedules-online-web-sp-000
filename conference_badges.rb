a8770f0bee3081919f6b963e22f56fc92cb94086
Jacob W Jones <jacobwadejones76@gmail.com>
2020-10-16 19:42:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,6 +13,7 @@ def assign_rooms
   rooms = [1, 2, 3, 4, 5, 6, 7]
   attendees.each do |name| rooms.each do |room|
     puts "Hello, #{name}! You'll be assigned to room #{room}!"
+  end 
 end
 
 def printer
