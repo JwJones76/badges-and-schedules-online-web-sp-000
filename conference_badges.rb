1b9cdcb10bb78ba127b1dcaaec7a69e68b2671e8
Jacob W Jones <jacobwadejones76@gmail.com>
2020-10-16 19:43:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,24 +1,3 @@
-def badge_maker(name)
-  "Hello, my name is #{name}."
-end
 
-def batch_badge_creator(attendees)
-  attendees.each do |name|
-    "Hello, my name is #{name}"
-  end 
-   
-end
-
-def assign_rooms
-  rooms = [1, 2, 3, 4, 5, 6, 7]
-  attendees.each do |name|
-  end 
-  rooms.each do |room|
-    puts "Hello, #{name}! You'll be assigned to room #{room}!"
-  end 
-end
-
-def printer
-end
 
 
