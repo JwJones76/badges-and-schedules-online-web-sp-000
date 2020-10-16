c46ce947310d1fd4a76c329dace759d436c5090f
Jacob W Jones <jacobwadejones76@gmail.com>
2020-10-16 19:19:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,12 +2,12 @@ names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 
 def badge_maker(name)
-  return "Hello, my name is #{name}."
+  p "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(names)
   names.each do |name|
-    puts "Hello, my name is #{name}."
+    p "Hello, my name is #{name}."
   end 
    
 end
